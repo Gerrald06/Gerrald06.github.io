1618a93100a51fc42ae412c0b759edec0c2db069
Gerrald06 <geraldsupitt@gmail.com>
2020-07-06 01:13:03 -0700

    add file javascript

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-// function change() {
-//     var image = document.getElementById('profile')
-//     image.src = 'facebook.jpg'
-// }
+var image_tracker ='foto/Profile Photo.jpg'
+function change() {
+    var image = document.getElementById('profile_pic')
+    if(image_tracker==='foto/Profile Photo.jpg') {
+        image.src = 'foto/Facebook.jpg'
+        image_tracker = 'foto/Profile Photo.jpg'
+    }
+    else{
+        image.src = 'foto/Facebook.jpg'
+        image_tracker = 'foto/Profile Photo.jpg'
+    }
+    image.src = 'foto/Facebook.jpg'
+}
 
+function changecolor(id) {
+    document.body.style.background = document.getElementById(id).innerHTML
+}
